1d05e6f643610e170bd24a4eac964a75813edb06
ArvikVan <arvikvan@gmail.com>
2022-06-06 10:33:02 +0300

    fix_Constructor

--- src/main/java/arv/consumefeignribbon/controller/UserController.java
+++ src/main/java/arv/consumefeignribbon/controller/UserController.java
@@ -4,16 +4,12 @@ import arv.consumefeignribbon.models.Message;
 import arv.consumefeignribbon.models.User;
 import arv.consumefeignribbon.models.UserDTO;
 import arv.consumefeignribbon.service.UsersService;
-import org.apache.catalina.LifecycleState;
-import org.atmosphere.config.service.Get;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 /**
  * @author ArvikV
@@ -23,24 +19,29 @@ import java.util.List;
 @RestController
 public class UserController {
 
+    private final UsersService usersService;
+
+    public UserController(UsersService usersService) {
+        this.usersService = usersService;
+    }
+
     @GetMapping("/getAll")
-    public ResponseEntity<User[]> getAll(UsersService usersService) {
+    public ResponseEntity<User[]> getAll() {
         return new ResponseEntity<>(usersService.getALLInfoUsers(), HttpStatus.OK);
     }
 
     @GetMapping("/getDTO")
-    public ResponseEntity<UserDTO[]> getALLUsersDTO(UsersService usersService) {
+    public ResponseEntity<UserDTO[]> getALLUsersDTO() {
         return new ResponseEntity<>(usersService.getUserDTO(), HttpStatus.OK);
     }
 
     @GetMapping("/{id}")
-    public ResponseEntity<UserDTO> getUserDTOById(UsersService usersService, @PathVariable Integer id) {
+    public ResponseEntity<UserDTO> getUserDTOById(@PathVariable Integer id) {
         return new ResponseEntity<>(usersService.getUserById(id), HttpStatus.OK);
     }
 
     @GetMapping("/getMessageInfo")
-    public ResponseEntity<Message> getMessageInfo(UsersService usersService,
-                                                  @RequestParam String senderMsid,
+    public ResponseEntity<Message> getMessageInfo(@RequestParam String senderMsid,
                                                   @RequestParam String creationDate) {
         return new ResponseEntity<>(usersService.getMessageInfo(senderMsid, creationDate), HttpStatus.OK);
     }
